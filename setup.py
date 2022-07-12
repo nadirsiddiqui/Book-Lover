f1b7dfbd07b8d410b2c4b4dea9f5173544fa77cb
Nadir Siddiqui <nadirsiddiqui@Nadirs-MacBook-Pro.local>
2022-07-12 02:54:06 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='Keep track of your reading!',
+      url='https://github.com/nadirsiddiqui/Book-Lover',
+      author='Nadir Siddiqui',
+      author_email='nhs2bc@virginia.edu',
+      license='MIT',
+      packages=['lover'])
